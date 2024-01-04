f3ecda5d69814aa9e23d793223b7c9ef37d664ee
mohima13 <modakmohima46@gmail.com>
2024-01-04 10:25:47 +0600

    make a unipue banner

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,37 +1,70 @@
 import Marquee from "react-fast-marquee";
-
+import './Home.css'
 const Home = () => {
   return (
-    <div>
+    <div className="grid grid-cols-3 gap-7 items-center">
       <Marquee speed={100}>
         <div className="absolute  inset-0 "></div>
         <img
-          src="https://images.pexels.com/photos/3368816/pexels-photo-3368816.jpeg?auto=compress&cs=tinysrgb&w=600"
-          className="h-[300px]"
+          src="https://images.pexels.com/photos/895223/pexels-photo-895223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="h-[550px]"
         />
         <img
-          src="https://images.pexels.com/photos/4170629/pexels-photo-4170629.jpeg?auto=compress&cs=tinysrgb&w=600"
-          className="h-[300px]"
+          src="https://images.pexels.com/photos/8845419/pexels-photo-8845419.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="h-[550px]"
           srcset=""
         />
         <img
-          src="https://images.pexels.com/photos/7171398/pexels-photo-7171398.jpeg?auto=compress&cs=tinysrgb&w=600"
-          className="h-[300px]"
+          src="https://images.pexels.com/photos/8188922/pexels-photo-8188922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="h-[550px]"
           srcset=""
         />
         <img
-          src="https://images.pexels.com/photos/4178791/pexels-photo-4178791.jpeg?auto=compress&cs=tinysrgb&w=600"
-          className="h-[300px]"
+          src="https://images.pexels.com/photos/8188925/pexels-photo-8188925.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="h-[550px]"
           srcset=""
         />
         <img
-          src="https://images.pexels.com/photos/3599208/pexels-photo-3599208.jpeg?auto=compress&cs=tinysrgb&w=600"
-          className="h-[300px]"
+          src="https://images.pexels.com/photos/7172069/pexels-photo-7172069.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="h-[550px]"
           srcset=""
         />
         <img
-          src="https://images.pexels.com/photos/8058761/pexels-photo-8058761.jpeg?auto=compress&cs=tinysrgb&w=600"
-          className="h-[300px]"
+          src="https://images.pexels.com/photos/5591732/pexels-photo-5591732.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="h-[550px]"
+          srcset=""
+        />
+      </Marquee>
+      <h1 className="text-center font text-4xl">Recipe Book</h1>
+      <Marquee speed={100}>
+        <div className="absolute  inset-0 "></div>
+        <img
+          src="https://images.pexels.com/photos/895223/pexels-photo-895223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="h-[550px]"
+        />
+        <img
+          src="https://images.pexels.com/photos/8845419/pexels-photo-8845419.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="h-[550px]"
+          srcset=""
+        />
+        <img
+          src="https://images.pexels.com/photos/8188922/pexels-photo-8188922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="h-[550px]"
+          srcset=""
+        />
+        <img
+          src="https://images.pexels.com/photos/8188925/pexels-photo-8188925.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="h-[550px]"
+          srcset=""
+        />
+        <img
+          src="https://images.pexels.com/photos/7172069/pexels-photo-7172069.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="h-[550px]"
+          srcset=""
+        />
+        <img
+          src="https://images.pexels.com/photos/5591732/pexels-photo-5591732.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          className="h-[550px]"
           srcset=""
         />
       </Marquee>
