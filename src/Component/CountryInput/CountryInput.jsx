7b8784aeed7517ce906c7ea22dc3f5c2c467a173
mohima13 <modakmohima46@gmail.com>
2024-01-05 21:50:57 +0600

    creat a new route for filter country wise foods data

---
+++ src/Component/CountryInput/CountryInput.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const CountryInput = () => {
+  return (
+    <div>
+      <label>
+        <select name="" id="">
+          <option value="All">All</option>
+          <option value="Japan">Japan</option>
+          <option value="France">France</option>
+          <option value="Mexico">Mexico</option>
+          <option value="Thailand">Thailand</option>
+          <option value="India">India</option>
+          <option value="Bangladesh">Bangladesh</option>
+          <option value="USA">USA</option>
+          <option value="Denmark">Denmark</option>
+          <option value="Australia">Australia</option>
+          <option value="Canada">Canada</option>
+          <option value="Germany">Germany</option>
+          <option value="Finland">Finland</option>
+        </select>
+      </label>
+    </div>
+  );
+};
+
+export default CountryInput;
--- src/Component/CountryWiseFood/CountryWiseFood.jsx
+++ src/Component/CountryWiseFood/CountryWiseFood.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Marquee from "react-fast-marquee";
 
 const CountryWiseFood = () => {
   const [countryWiseFood, setCountryWiseFood] = useState([]);
@@ -10,17 +11,23 @@ const CountryWiseFood = () => {
   }, []);
 
   return (
-    <div>
-      <h1>CountryWiseFood {countryWiseFood.length}</h1>
-      <div>
+    <div className="">
+      <h1 className="text-4xl font-bold text-center p-16">
+        Country Wise Food: {countryWiseFood.length}
+      </h1>
+      <div className="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-5">
         {countryWiseFood.map((country) => (
-          <div key={country.country}>
-            <img src={country.food_image} alt="" />
-            <h1>{country.country}</h1>
-            <h1>{country.food_name}</h1>
-            <h1>{country.ingredients}</h1>
-            <h1>{country.instructions}</h1>
-            <h1>{country.duration}</h1>
+          <div key={country.country} className="border rounded-md relative ">
+            <img
+              src={country.food_image}
+              alt={country.food_name}
+              className="rounded-md sm:h-[260px] md:h-[150px] lg:h-[200px] transition-transform transform hover:scale-105"
+            />
+
+            <div className="absolute inset-0 flex flex-col justify-center items-center font-bold text-white backdrop-blur-lg opacity-0 hover:opacity-100 ">
+              <h1>{country.country}</h1>
+              <h2 className="text-center">{country.food_name}</h2>
+            </div>
           </div>
         ))}
       </div>
